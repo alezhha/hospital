44ad63a017151b9b20460611a35f97e961fc6337
Alezhha <callermanjohnson@gmail.com>
2021-09-23 16:11:01 +0600

    added some visual in html

---
+++ main/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Maindoctor, Patient, Doctor, Nurse
+
+class AddPatient(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+
+
+class AddNurses(forms.ModelForm):
+    class Meta:
+        model = Nurse
+        fields = "__all__"
+
+class AddMaindoctor(forms.ModelForm):
+    class Meta:
+        model = Maindoctor
+        fields = "__all__"
+
+class AddDoctor(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        fields = "__all__"
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Hospital
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"hospitals":Hospital.objects.all()})
